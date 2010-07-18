6423009587bfd1bee709aff1c06f4a467abdc38f
Alex Popescu <the.mindstorm@gmail.com>
2010-07-18 17:52:40 +0300

    small fix to tweet parse

--- twitter.conversation.js
+++ twitter.conversation.js
@@ -74,15 +74,15 @@ var twittrConv = {
 
   
   parse_tweet:function(text) {
-    text=text.replace(/http(s)?:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/, function(url) {
+    text=text.replace(/http(s)?:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) {
   		return url.link(url);
     });
     
-    text = text.replace(/[@]+[A-Za-z0-9-_]+/, function(u) {
+    text = text.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
 		  var username = u.replace("@","")
 		  return u.link("http://twitter.com/"+username);
 	  });
-    text = text.replace(/[#]+[A-Za-z0-9-_]+/, function(t) {
+    text = text.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
   		var tag = t.replace("#","%23")
 	  	return t.link("http://search.twitter.com/search?q="+tag);
     });
